fff567e57adf26b3916e9eaff7701855dca45b98
David Salgado <david@digitalronin.com>
2016-12-27 13:56:38 +0000

    hide previous estimates until estimation is complete
    coders should not be able to see each others' estimates until
    everyone has estimated the ticket

--- web/views/ticket_view.ex
+++ web/views/ticket_view.ex
@@ -1,3 +1,7 @@
 defmodule PlanningPoker.TicketView do
   use PlanningPoker.Web, :view
+
+  alias PlanningPoker.TicketUpdater
+
+  def estimation_complete?(ticket), do: TicketUpdater.estimation_complete?(ticket)
 end
