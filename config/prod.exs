1d717674b68a0e65d5516ca4119bd1f442a7e004
David Salgado <david@digitalronin.com>
2016-12-28 12:32:43 +0000

    setup for heroku deployment
    Followed instructions from here;
    
    https://medium.com/peep-stack/building-a-performant-web-app-with-ember-fastboot-and-phoenix-part-1-fa1241654308#.sr5c15l8r

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,15 @@ use Mix.Config
 # which you typically run after static files are built.
 config :planning_poker, PlanningPoker.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "gentle-waters-41201.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
+
+config :planning_poker, PlanningPoker.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
