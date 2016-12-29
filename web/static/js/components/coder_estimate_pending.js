97c1e55324908560e21524e080cbc73536b65674
David Salgado <david@digitalronin.com>
2016-12-29 12:45:13 +0000

    add pending estimate components
    render all estimate components on the page

---
+++ web/static/js/components/coder_estimate_pending.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+var CoderEstimatePending = React.createClass({
+  render() {
+    return(
+      <div className="estimate">
+        <div className="estimate-card estimate-pending">
+          <span className="coder">Jesus</span>
+        </div>
+
+        <div className="estimate-card estimate-choice"> <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Jesus/1">      1 </a>  </div>
+        <div className="estimate-card estimate-choice"> <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Jesus/2">      2 </a>  </div>
+        <div className="estimate-card estimate-choice"> <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Jesus/3">      3 </a>  </div>
+        <div className="estimate-card estimate-choice"> <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Jesus/5">      5 </a>  </div>
+        <div className="estimate-card estimate-choice"> <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Jesus/8">      8 </a>  </div>
+        <div className="estimate-card estimate-choice"> <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Jesus/13">    13 </a>  </div>
+      </div>
+    )
+  }
+})
+
+module.exports = CoderEstimatePending
+
--- web/static/js/components/coder_estimates.js
+++ web/static/js/components/coder_estimates.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import CoderEstimateHidden from "./coder_estimate_hidden"
 import CoderEstimateCompleted from "./coder_estimate_completed"
+import CoderEstimatePending from "./coder_estimate_pending"
 
 var CoderEstimates = React.createClass({
   propTypes: {
@@ -11,13 +12,22 @@ var CoderEstimates = React.createClass({
   render() {
     var rtn
 
-    if (this.props.estimateComplete) {
-      rtn = <CoderEstimateCompleted />
-    } else {
-      rtn = <CoderEstimateHidden />
-    }
+    // if (this.props.estimateComplete) {
+    //   rtn = <CoderEstimateCompleted />
+    // } else {
+    //   rtn = <CoderEstimateHidden />
+    // }
+    //
+    // return <div className="estimates">{rtn}<hr /></div>
 
-    return <div className="estimates">{rtn}<hr /></div>
+    return(
+      <div className="estimates">
+        <CoderEstimateHidden />
+        <CoderEstimateCompleted />
+        <CoderEstimatePending />
+        <hr />
+      </div>
+    )
   }
 })
 
