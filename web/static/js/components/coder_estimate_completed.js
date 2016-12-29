dbbaa8372c309043d7807dd7bda5e5d6d205adfd
David Salgado <david@digitalronin.com>
2016-12-29 12:51:03 +0000

    attach click events to hidden & completed estimate cards

--- web/static/js/components/coder_estimate_completed.js
+++ web/static/js/components/coder_estimate_completed.js
@@ -4,12 +4,17 @@ var CoderEstimateCompleted = React.createClass({
   render() {
     return(
       <div className="estimate">
-        <div className="estimate-card estimate-completed">
-          <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/Christian/0"><span className="coder">Christian</span>: 2</a>
+        <div className="estimate-card estimate-completed" onClick={this.handleClick}>
+          <span className="coder">Christian</span>: 2
         </div>
       </div>
     )
+  },
+
+  handleClick() {
+    console.log('CoderEstimateCompleted change my estimate')
   }
+
 })
 
 module.exports = CoderEstimateCompleted
--- web/static/js/components/coder_estimate_hidden.js
+++ web/static/js/components/coder_estimate_hidden.js
@@ -4,13 +4,17 @@ var CoderEstimateHidden = React.createClass({
   render() {
     return(
       <div className="estimate">
-        <div className="estimate-card estimate-hidden">
-          <a href="/tickets/9bd7e27d-2e36-4f15-871b-2f3b4e198aef/estimate/David/0"><span className="coder">David</span>: ?
-          </a>
+        <div className="estimate-card estimate-hidden" onClick={this.handleClick}>
+          <span className="coder">David</span>: ?
         </div>
       </div>
     )
+  },
+
+  handleClick() {
+    console.log('CoderEstimateHidden change my estimate')
   }
+
 })
 
 module.exports = CoderEstimateHidden
