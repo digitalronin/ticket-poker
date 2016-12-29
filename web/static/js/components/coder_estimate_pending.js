4e8e3147de4e2454c3f961a12a75b24ba9edde82
David Salgado <david@digitalronin.com>
2016-12-29 13:18:01 +0000

    build estimate choices from pointOptions and coder properties

--- web/static/js/components/coder_estimate_pending.js
+++ web/static/js/components/coder_estimate_pending.js
@@ -5,19 +5,20 @@ import EstimateChoice from "./estimate_choice"
 // TODO: pass in coder and point options
 
 var CoderEstimatePending = React.createClass({
+  propTypes: {
+    coder: React.PropTypes.string,
+    pointOptions: React.PropTypes.array
+  },
+
   render() {
+    var choices = this.props.pointOptions.map((p) => { return <EstimateChoice coder={this.props.coder} key={p} value={p} /> })
+
     return(
       <div className="estimate">
         <div className="estimate-card estimate-pending">
-          <span className="coder">Jesus</span>
+          <span className="coder">{this.props.coder}</span>
         </div>
-
-        <EstimateChoice coder="Jesus" value={1} />
-        <EstimateChoice coder="Jesus" value={2} />
-        <EstimateChoice coder="Jesus" value={3} />
-        <EstimateChoice coder="Jesus" value={5} />
-        <EstimateChoice coder="Jesus" value={8} />
-        <EstimateChoice coder="Jesus" value={13} />
+        {choices}
       </div>
     )
   }
--- web/static/js/components/coder_estimates.js
+++ web/static/js/components/coder_estimates.js
@@ -4,14 +4,16 @@ import CoderEstimateHidden from "./coder_estimate_hidden"
 import CoderEstimateCompleted from "./coder_estimate_completed"
 import CoderEstimatePending from "./coder_estimate_pending"
 
+// TODO: pass in the state as estimates hash and pointOptions array
+
 var CoderEstimates = React.createClass({
   propTypes: {
     estimateComplete: React.PropTypes.bool
   },
 
   render() {
-    var rtn
-
+    // var rtn
+    //
     // if (this.props.estimateComplete) {
     //   rtn = <CoderEstimateCompleted />
     // } else {
@@ -20,11 +22,13 @@ var CoderEstimates = React.createClass({
     //
     // return <div className="estimates">{rtn}<hr /></div>
 
+    var pointOptions = [1, 2, 3, 4, 5]
+
     return(
       <div className="estimates">
         <CoderEstimateHidden />
         <CoderEstimateCompleted />
-        <CoderEstimatePending />
+        <CoderEstimatePending coder="Jesus" pointOptions={pointOptions} />
         <hr />
       </div>
     )
