01fe5f448ee819c334e8b144646654fe03800c89
David Salgado <david@digitalronin.com>
2016-12-29 13:40:14 +0000

    WiP: render estimates according to different values

--- web/static/js/components/coder_estimates.js
+++ web/static/js/components/coder_estimates.js
@@ -5,33 +5,47 @@ import CoderEstimateCompleted from "./coder_estimate_completed"
 import CoderEstimatePending from "./coder_estimate_pending"
 
 // TODO: pass in the state as estimates hash and pointOptions array
+// TODO: work out whether or not the estimate is complete
+// TODO: get the ticket data from the server
+// TODO: push new ticket data to the server
 
 var CoderEstimates = React.createClass({
   propTypes: {
-    estimateComplete: React.PropTypes.bool
+    estimateComplete: React.PropTypes.bool,
+    estimates: React.PropTypes.object,
+    pointOptions: React.PropTypes.array
   },
 
   render() {
-    // var rtn
-    //
-    // if (this.props.estimateComplete) {
-    //   rtn = <CoderEstimateCompleted />
-    // } else {
-    //   rtn = <CoderEstimateHidden />
-    // }
-    //
-    // return <div className="estimates">{rtn}<hr /></div>
+    console.log('CoderEstimates', this.props.estimates, this.props.pointOptions);
 
-    var pointOptions = [1, 2, 3, 4, 5]
+    var alice   = this.estimateRow(false, "Alice", 3)
+    var bob     = this.estimateRow(true, "Bob", 7)
+    var charlie = this.estimateRow(false, "Charlie", 0)
 
     return(
       <div className="estimates">
-        <CoderEstimateHidden />
-        <CoderEstimateCompleted />
-        <CoderEstimatePending coder="Jesus" pointOptions={pointOptions} />
+        {alice}
+        {bob}
+        {charlie}
         <hr />
       </div>
     )
+  },
+
+  estimateRow(estimateComplete, coder, points) {
+    console.log('estimateCard', estimateComplete, coder, points);
+    var rtn
+
+    if (points === 0) {
+      rtn = <CoderEstimatePending coder={coder} pointOptions={this.props.pointOptions} />
+    } else {
+      rtn = (estimateComplete)
+            ? <CoderEstimateCompleted coder={coder} points={points} />
+            : <CoderEstimateHidden coder={coder} />
+    }
+
+    return rtn
   }
 })
 
