a53e91d4669523ce1ff4e9e4dab582ab409355d1
David Salgado <david@digitalronin.com>
2016-12-29 12:04:53 +0000

    nested react components

---
+++ web/static/js/components/foo.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+var Foo = React.createClass({
+  render() {
+    return(
+      <h2>Hello Foo</h2>
+    )
+  }
+})
+
+module.exports = Foo
+
--- web/static/js/components/hello.js
+++ web/static/js/components/hello.js
@@ -1,9 +1,14 @@
 import React from "react"
 
+import Foo from "./foo"
+
 var HelloWorld = React.createClass({
   render() {
     return(
-      <h1>Hello React</h1>
+      <div>
+        <h1>Hello React</h1>
+        <Foo />
+      </div>
     )
   }
 })
