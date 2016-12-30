c6c0e3443a06cad76396232a65c741376cb96e91
David Salgado <david@digitalronin.com>
2016-12-30 13:26:36 +0000

    add more ticket channel tests
    confirm that the ticket data is sent over the channel

--- test/channels/ticket_channel_test.exs
+++ test/channels/ticket_channel_test.exs
@@ -1,7 +1,14 @@
 defmodule TicketPoker.TicketChannelTest do
   use TicketPoker.ChannelCase
 
-  alias TicketPoker.TicketChannel
+  alias TicketPoker.{TicketChannel, TeamUpdater}
+
+  @team_attrs %{
+    "coders"         => ["a", "b", "c"],
+    "name"           => "team name",
+    "points_string"  => "1 2 3",
+    "new_ticket_url" => "http://foo.bar.baz"
+  }
 
   @ticket_id "35bf6df4-69a5-4bd3-a9e5-dddd7d55e3ea"
 
@@ -14,8 +21,27 @@ defmodule TicketPoker.TicketChannelTest do
   end
 
   test "update broadcasts to channel", %{socket: socket} do
-    push socket, "update", %{}
-    assert_broadcast "update", %{}
+    {:ok, _team, ticket} = TeamUpdater.create(@team_attrs)
+
+    message = %{ "ticket_id" => ticket.id, "coder" => "a", "points" => 5 }
+    push socket, "update", message
+
+    assert_broadcast "update", %{
+      point_options: [1, 2, 3],
+      estimates:     %{ "a" => 5, "b" => 0, "c" => 0 }
+    }
+  end
+
+  test "get_ticket fetches ticket state", %{socket: socket} do
+    {:ok, _team, ticket} = TeamUpdater.create(@team_attrs)
+
+    message = %{ "ticket_id" => ticket.id }
+    push socket, "get_ticket", message
+
+    assert_push "update", %{
+      point_options: [1, 2, 3],
+      estimates:     %{ "a" => 0, "b" => 0, "c" => 0 }
+    }
   end
 
   test "broadcasts are pushed to the client", %{socket: socket} do
