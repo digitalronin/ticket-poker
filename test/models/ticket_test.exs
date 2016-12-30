ea6c4869d95401dfc7e6ea1430779666f0d9b3b7
David Salgado <david@digitalronin.com>
2016-12-30 13:29:11 +0000

    add a test for Ticket.to_map

--- test/models/ticket_test.exs
+++ test/models/ticket_test.exs
@@ -15,4 +15,18 @@ defmodule TicketPoker.TicketTest do
     changeset = Ticket.changeset(%Ticket{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "to_map" do
+    ticket = %Ticket{
+      id:             "foo",
+      url:            "bar",
+      point_options:  [1, 2, 3],
+      estimates:      %{ "a" => 2, "b" => 3 }
+    }
+
+    assert Ticket.to_map(ticket) == %{
+      estimates:      %{"a" => 2, "b" => 3},
+      point_options:  [1, 2, 3]
+    }
+  end
 end
